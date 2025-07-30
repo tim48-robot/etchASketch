0a45e41e9aac1a27387a60b3653ae8a5c606d21e
tim48-robot <justintimothy303@gmail.com>
2025-07-30 10:23:12 +0700

    fix: fixed space allocation for new square grid

--- scripts.js
+++ scripts.js
@@ -24,9 +24,10 @@ function createGrid(userInput){
         for (let i=0; i<userInput; i++){
             let square = document.createElement("div");
             square.style.padding = `${16/userInput * 16}px`;
-            square.style.borderWidth = `${16/userInput * 2}px`;
-            square.style.borderColor = "black";
-            square.style.borderStyle = "solid";
+            // square.style.borderWidth = `${16/userInput * 2}px`;
+            // square.style.borderColor = "black";
+            // square.style.borderStyle = "solid";
+            square.style.outline = `${16/userInput * 2}px solid black `
             square.classList.add("square");
             newDiv.appendChild(square);
         }
